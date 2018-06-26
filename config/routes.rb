3a8b012d8cd1e30686fb37588027e507d7596c99
EduardoRicSan <lalo19jers.azul@gmail.com>
2018-06-26 15:03:33 -0500

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+ # get 'sessions/new'
+
+  #get 'sessions/create'
+
+  #get 'sessions/destroy'
+
+  resources :users
+  resources :products do
+    get :download, :on => :member
+    get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
